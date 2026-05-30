95dacaf683d0387657b376933629e770f67b793a
Davi <davi.xa.oliveira@gmail.com>
2026-05-29 22:26:07 -0300

    Calcula o índice de massa corporal de uma pesso

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿double altura, peso , imc;
+
+Console.Write("Digite sua Altura: Altura (m)..: ");
+
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite seu Peso: Peso (kg)...: ");
+
+peso = Convert.ToDouble(Console.ReadLine());
+
+imc = peso / Math.Pow(altura, 2);
+
+Console.WriteLine($"\nIMC: {imc:N1} kg/m²");
